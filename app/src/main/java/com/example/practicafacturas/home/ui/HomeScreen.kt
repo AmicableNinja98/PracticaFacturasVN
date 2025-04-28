d0fc4edbdeb06bb6f1bf9b43ade6c41735d67510
MARIO RUIZ TORRES <6003466@viewnext.com>
2025-04-28 09:17:23 +0200

    Cambios en la Home Screen

--- app/src/main/java/com/example/practicafacturas/home/ui/HomeScreen.kt
+++ app/src/main/java/com/example/practicafacturas/home/ui/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.practicafacturas.home.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -25,14 +26,19 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.practicafacturas.R
 import com.example.practicafacturas.home.usecase.HomeScreenViewModel
 
@@ -46,52 +52,87 @@ fun HomeScreen(
         snackbarHost = {
             SnackbarHost(homeScreenViewModel.snackbarHostState)
         }
+    ) { innerPadding ->
+        HomeScreenBody(
+            homeScreenViewModel,
+            onNavigateToFacturas,
+            onNavigateToSmartSolar,
+            Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun HomeScreenBody(
+    homeScreenViewModel: HomeScreenViewModel,
+    onNavigateToFacturas: (Boolean) -> Unit,
+    onNavigateToSmartSolar: () -> Unit,
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+            .padding(vertical = 8.dp, horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color.White)
-                .padding(innerPadding),
+        Text(
+            text = stringResource(R.string.homeScreen_title),
+            fontWeight = FontWeight.Bold,
+            fontSize = 40.sp,
+            modifier = Modifier.padding(top = 12.dp, bottom = 24.dp)
+        )
+        Image(painter = painterResource(R.drawable.logoiberdrola),contentDescription = null)
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp),
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            Text(
-                text = stringResource(R.string.homeScreen_title),
-                style = MaterialTheme.typography.headlineSmall,
-                modifier = Modifier.padding(top = 12.dp,bottom = 24.dp)
-            )
-            Text(text = "Usar datos Mock: ${if(homeScreenViewModel.useMockData) "Si" else "No"}")
-            Switch(
-                checked = homeScreenViewModel.useMockData,
-                onCheckedChange = {
-                        value ->
-                    homeScreenViewModel.onSwitchCheckedChange(value)
-                },
-            )
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(2),
-                modifier = Modifier.fillMaxSize(),
-                contentPadding = PaddingValues(8.dp),
-                verticalArrangement = Arrangement.spacedBy(12.dp),
-                horizontalArrangement = Arrangement.spacedBy(12.dp)
-            ) {
-                item {
-                    HomeCard(
-                        title = stringResource(R.string.homeScreen_facturasCard_title),
-                        icon = Icons.AutoMirrored.Filled.List,
-                        onClick = {
-                            onNavigateToFacturas(homeScreenViewModel.useMockData)
-                        }
-                    )
-                }
-                item {
-                    HomeCard(
-                        title = stringResource(R.string.homeScreen_smartSolarCard_title),
-                        icon = Icons.Filled.Star,
-                        onClick = onNavigateToSmartSolar
-                    )
-                }
+            item {
+                HomeCard(
+                    title = stringResource(R.string.homeScreen_facturasCard_title),
+                    icon = Icons.AutoMirrored.Filled.List,
+                    onClick = {
+                        onNavigateToFacturas(homeScreenViewModel.useMockData)
+                    }
+                )
+            }
+            item {
+                HomeCard(
+                    title = stringResource(R.string.homeScreen_smartSolarCard_title),
+                    icon = Icons.Filled.Star,
+                    onClick = onNavigateToSmartSolar
+                )
             }
         }
+        Text(text = "Usar datos Mock: ${if (homeScreenViewModel.useMockData) "Si" else "No"}")
+        Switch(
+            checked = homeScreenViewModel.useMockData,
+            onCheckedChange = { value ->
+                homeScreenViewModel.onSwitchCheckedChange(value)
+            },
+            colors = SwitchColors(
+                checkedThumbColor = Color.White,
+                checkedTrackColor = colorResource(R.color.light_orange),
+                checkedBorderColor = Color.Black,
+                checkedIconColor = colorResource(R.color.light_orange),
+                uncheckedThumbColor = Color.White,
+                uncheckedTrackColor = Color.LightGray,
+                uncheckedBorderColor = Color.Black,
+                uncheckedIconColor = Color.Black,
+                disabledCheckedThumbColor = Color.Black,
+                disabledCheckedTrackColor = Color.Black,
+                disabledCheckedBorderColor = Color.Black,
+                disabledCheckedIconColor = Color.Black,
+                disabledUncheckedThumbColor = Color.Black,
+                disabledUncheckedTrackColor = Color.Black,
+                disabledUncheckedBorderColor = Color.Black,
+                disabledUncheckedIconColor = Color.Black
+            )
+        )
     }
 }
 
@@ -107,11 +148,11 @@ fun HomeCard(title: String, icon: ImageVector, onClick: () -> Unit) {
     ) {
         Column(
             modifier = Modifier
-                .padding(16.dp),
+                .padding(16.dp).fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Icon(imageVector = icon, contentDescription = title, modifier = Modifier.size(48.dp))
+            Icon(imageVector = icon, contentDescription = title, modifier = Modifier.size(60.dp))
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = title, style = MaterialTheme.typography.bodyLarge)
         }
